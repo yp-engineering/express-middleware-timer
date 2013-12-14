f8a2d9c5c60b79ba400204fb0768cee14f5f4d42
Josh Mervine <jmervine@yp.com>
2013-12-14 14:20:47 -0800

    Updating example.

--- examples/custom_report.js
+++ examples/custom_report.js
@@ -33,18 +33,34 @@ app.use(emt.instrument(fastMiddleware, 'fastMiddleware'));
 /***
  * Custom Reporting
  ***************************************************/
-var fs = require('fs');
+var fs     = require('fs');
+var format = require('util').format;
 app.use(function(req, res, next) {
     // Call next right away for performance.
     next();
 
     // Write report to file.
     var report = emt.calculate(req, res);
-    fs.writeFile('emt.log', JSON.stringify(report, null, 2), function(err) {
+
+    var out = format('%s,%s,%s,%s\n',
+                new Date(),
+                report.request.url,
+                report.timers.slowMiddleware.took,
+                report.timers.fastMiddleware.took);
+
+    fs.appendFile('emt.log', out, function(err) {
         if (err) console.trace(err);
     });
 
-    // You can also do things like save report to mongo or redis.
+    /***
+     * Outputs something like the following...
+
+     * Sat Dec 14 2013 14:17:00 GMT-0800 (PST),/,202,10
+     * Sat Dec 14 2013 14:17:02 GMT-0800 (PST),/,201,2
+     * Sat Dec 14 2013 14:17:03 GMT-0800 (PST),/,200,1
+     *
+     * You can also do things like save report to mongo or redis.
+     */
 });
 
 /***
